67a8ea71564eaab7fb4dfbb662561d7814eea29d
AdejinmiSeyi <seyifeyisara@gmail.com>
2022-05-13 13:34:00 +0100

    Password generator app

---
+++ index.js
@@ -0,0 +1,122 @@
+/*
+const lowerCaseLetters = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const numbersZeroToNine = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const selectedSpecialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+*/
+
+let randomChar = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "+",
+  "-",
+  ".",
+  "`",
+  "~",
+  "|",
+  "<",
+  ">",
+  "=",
+  "-",
+  "_",
+  ",",
+];
+
+let generateBtn = document.getElementById("generate");
+
+generateBtn.addEventListener("click", () => generator(15));
+
+let box1 = document.getElementById("box1");
+let box2 = document.getElementById("box2");
+let box3 = document.getElementById("box3");
+let box4 = document.getElementById("box4");
+
+let boxes = [box1, box2, box3, box4];
+
+function generator(length) {
+  for (let i = 0; i < boxes.length; i++) {
+    let random = "";
+
+    for (let j = 0; j < length; j++) {
+      //let random = ""
+
+      //console.log(random)
+      random += randomChar[Math.floor(Math.random() * randomChar.length)];
+      //console.log(random)
+    }
+
+    boxes[i].innerHTML = random;
+  }
+
+  return boxes;
+}
